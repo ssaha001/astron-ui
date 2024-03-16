9fb77a7722bdc013a261541bb887b17079a01c18
Cristo <crtcristo@gmail.com>
2024-03-15 20:58:02 -0400

    bacground and text changes

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom"; // Import the Link component
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./LandingPage.css"
 function LandingPage() {
   return (
 <div>
   <Container>
         <Row className="mt-5">
           <Col className="text-center">
-            <h1>Stay on top of your building</h1>
-            <h1>projects with us!</h1>
+            <h1 id="slogan">Stay on top of your building</h1>
+            <h1 id="slogan">projects with us!</h1>
             <img src="https://live.staticflickr.com/65535/53588740710_3721fd3c5a_b.jpg" width="800" className="img-fluid" alt="A roof"/>
             <br/>
             <br/>
