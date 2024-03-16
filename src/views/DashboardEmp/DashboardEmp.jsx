64ea1ee5d39f70019edf1cfc76ce8929fab687e6
Eleana <emita@myseneca.ca>
2024-03-15 23:35:22 -0400

    I made the employee schedule table and made it show up at the page

--- src/views/DashboardEmp/DashboardEmp.jsx
+++ src/views/DashboardEmp/DashboardEmp.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import EmployeeTable from "../../Tables/EmployeeTable"; // Import the Table component
+
 const DashboardEmp=()=>{
 return(<>
-This is employee page.
-</>)
-}
-
+      <h1>Employee Dashboard</h1>
+      <EmployeeTable data={EmployeeTable}/> 
+</>
+);
+};
 export default DashboardEmp
